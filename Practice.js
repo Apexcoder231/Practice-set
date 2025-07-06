5396953920b1767ee065c4ead513b4cb622cecaf
Duman <dumanmuduli@gmail.com>
2025-07-06 15:01:25 +0530

    js_start

---
+++ Practice.js
@@ -0,0 +1,17 @@
+const name = 'suman'
+var age = "20";
+let isStudent = true;
+hobbies = ['reading', 'coding', 'gaming'];
+
+console.log(`My name is ${name}, I am ${age} years old, and it is ${isStudent} that I am a student.`);
+
+console.table([{ name, age, isStudent, hobbies }]);
+
+console.log(typeof age);
+
+
+// number ,bigint , string , boolean , null , undefined , symbol - primitive data types
+
+// object , array - non-primitive data types
+
+
