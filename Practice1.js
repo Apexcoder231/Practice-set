c0d17d4b117c9916cc65f856e2abf307dbebef42
Duman <dumanmuduli@gmail.com>
2025-07-06 15:23:30 +0530

    practice1

---
+++ Practice1.js
@@ -0,0 +1,12 @@
+let score = "33abc";
+
+// console.log(typeof score); // string
+// console.log(typeof (score ));
+
+let valueOfScore = Number(score);
+// console.log(typeof valueOfScore); // number
+// console.log(valueOfScore); // 33
+
+let loggedIn = "1";
+let isLoggedIn = Boolean(loggedIn);
+console.log(isLoggedIn); // false
